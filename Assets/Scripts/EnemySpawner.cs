19eba95cebc93cf9b5f4544b7fd6ab9b88516d90
Marcus P <marcuspruks@hotmail.com>
2024-03-29 19:03:21 +0200

    enemyspawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemySpawner : MonoBehaviour {
+    [Header("References")]
+    [SerializeField] private GameObject[] enemyPrefabs;
+    
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+    [SerializeField] private float timeBetweenWaves = 5f;
+    [SerializeField] private float scalingFactor = 0.75f;
+
+    [Header("Events")]
+    public static UnityEvent onEnemyDestroy = new UnityEvent();
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeftToSpawn;
+    private bool isSpawning = false;
+
+    private void Awake() {
+        onEnemyDestroy.AddListener(EnemyDestroyed);
+    }
+
+    private void EnemyDestroyed() {
+        enemiesAlive--;
+    }
+
+    private void Start() {
+        StartWave();
+    }
+
+    private void StartWave() {
+        isSpawning = true;
+        enemiesLeftToSpawn = baseEnemies;
+    }
+
+    private void Update() {
+        if (!isSpawning) return;
+
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0) {
+            SpawnEnemy();
+            enemiesLeftToSpawn--;
+            enemiesAlive++;
+            timeSinceLastSpawn = 0f;
+        }
+    }
+    
+    private void SpawnEnemy() {
+        GameObject prefabToSpawn = enemyPrefabs[0];
+        Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+    }
+
+    private int EnemiesPerWave() {
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, scalingFactor));
+    }
+    
+}
