e21d4a5e8761d2fd29691e4e79da61b12a6a3b4f
Marcus P <marcuspruks@hotmail.com>
2024-03-30 10:45:12 +0200

    Changed Menu loc, created bullshit tower 1, menu pos movement, money :)

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class Menu : MonoBehaviour {
+    [Header("References")]
+    [SerializeField] TextMeshProUGUI currencyUI;
+
+    private bool isMenuOpen = true;
+
+    public void ToggleMenu() {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        // Do the reverse animation
+        if (isMenuOpen) {
+            rectTransform.DOMove(new Vector3(0, -200, 0), 0.5f).SetEase(Ease.Linear);
+        } else {
+            rectTransform.DOMove(new Vector3(0, 0, 0), 0.5f).SetEase(Ease.Linear);
+            
+        }
+
+        isMenuOpen = !isMenuOpen;
+    }
+
+    private void OnGUI(){
+        currencyUI.text = LevelManager.main.currency.ToString();
+    }
+}
