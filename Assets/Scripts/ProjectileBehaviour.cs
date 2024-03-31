a8a9a1b19d73d09d4eb7540ccd32ba7e790d0391
Ekke-Markus Muttika <ekkemuttika@gmail.com>
2024-03-31 08:46:53 +0300

    kolju hääl

--- Assets/Scripts/ProjectileBehaviour.cs
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -7,6 +7,7 @@ public class ProjectileBehaviour : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private float lifeLength = 5.0f;
     private float lifeStartTime;
+    public GameObject crunchSound;
 
     // Target object towards which the projectile should move
     private GameObject targetObject;
@@ -69,7 +70,7 @@ public class ProjectileBehaviour : MonoBehaviour
     {
         if (collision.tag == "Monster")
         {
-           
+            GameObject.Instantiate(crunchSound, collision.transform);
             collision.GetComponent<MonsterBehavior>().ProjectileHit();
             DestroyObject();
         }
